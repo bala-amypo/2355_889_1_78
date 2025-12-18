f2ba856d6616c4423799e33a7d7f8cb7e151f1f7
bala-amypo <bala@amypo.in>
2025-12-18 08:35:35 +0000

    User commit

--- src/main/java/com/example/demo/model/VolunteerProfile.java
+++ src/main/java/com/example/demo/model/VolunteerProfile.java
@@ -1,9 +1,16 @@
 package com.example.demo.model;
+import jakarta.persistence.*;
 public class VolunteerProfile{
+    @Id
     private Long id;
     private String  volunteerId;
     private String fullName;
+    @Column(unique=true)
     private String email;
     private String phone,
-    private S
+    private String AVAILABLE;
+    private String BUSY;
+    private String INACTIVE;
+    private LocalDateTime createdAt;
+    
 }
