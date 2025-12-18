d406d90ced9120d2753334590a7274b537f6760d
bala-amypo <bala@amypo.in>
2025-12-18 08:40:35 +0000

    User commit

--- src/main/java/com/example/demo/model/VolunteerSkillRecord.java
+++ src/main/java/com/example/demo/model/VolunteerSkillRecord.java
@@ -1,4 +1,91 @@
 package com.example.demo.model;
-public class VolunteerSkillRecord{
-    
+
+import java.time.*;
+import jakarta.persistence.*;
+public class VolunteerSkillRecord {
+
+    @Id
+    private Long id;
+
+    private String taskCode;
+    private String taskName;
+
+    private String requiredSkill;
+
+    private String ;
+    private String AVAILABLE;
+    private String BUSY;
+    private String INACTIVE;
+
+    private LocalDateTime createdAt;
+    public VolunteerProfile() {
+    }
+    public VolunteerProfile(Long id, String volunteerId, String fullName,
+                            String email, String phone,
+                            String AVAILABLE, String BUSY,
+                            String INACTIVE, LocalDateTime createdAt) {
+        this.id = id;
+        this.volunteerId = volunteerId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.AVAILABLE = AVAILABLE;
+        this.BUSY = BUSY;
+        this.INACTIVE = INACTIVE;
+        this.createdAt = createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getVolunteerId() {
+        return volunteerId;
+    }
+    public void setVolunteerId(String volunteerId) {
+        this.volunteerId = volunteerId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getAVAILABLE() {
+        return AVAILABLE;
+    }
+    public void setAVAILABLE(String AVAILABLE) {
+        this.AVAILABLE = AVAILABLE;
+    }
+    public String getBUSY() {
+        return BUSY;
+    }
+    public void setBUSY(String BUSY) {
+        this.BUSY = BUSY;
+    }
+    public String getINACTIVE() {
+        return INACTIVE;
+    }
+    public void setINACTIVE(String INACTIVE) {
+        this.INACTIVE = INACTIVE;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
