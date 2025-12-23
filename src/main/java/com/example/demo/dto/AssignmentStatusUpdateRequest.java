1a855b4d8decb2b22367478dc94e6e33d10803d3
bala-amypo <bala@amypo.in>
2025-12-23 06:26:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AssignmentStatusUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto;
+
+public class AssignmentStatusU{
+    
+    private String status;
+    
+    // TODO: Add constructors
+    
+    
+    // TODO: Add getters and setters
+    
+    
+}
