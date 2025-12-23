9ae2031403c2dca26d1e73b3541260f5069efc45
bala-amypo <bala@amypo.in>
2025-12-23 05:14:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.AssignmentEvaluationRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface AssignmentEvaluationRecordRepository
-        extends JpaRepository<AssignmentEvaluationRecord, Long> {
-
+@Repository
+public interface AssignmentEvaluationRecordRepository extends JpaRepository<AssignmentEvaluationRecord, Long> {
     List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
 }
--- src/main/java/com/example/demo/repository/TaskAssignmentRecordRepository.java
+++ src/main/java/com/example/demo/repository/TaskAssignmentRecordRepository.java
@@ -1,13 +1,14 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.TaskAssignmentRecord;
+import com.example.demo.model.TaskAssignmentRecord.AssignmentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface TaskAssignmentRecordRepository
-        extends JpaRepository<TaskAssignmentRecord, Long> {
-
+@Repository
+public interface TaskAssignmentRecordRepository extends JpaRepository<TaskAssignmentRecord, Long> {
     List<TaskAssignmentRecord> findByVolunteerId(Long volunteerId);
-
     List<TaskAssignmentRecord> findByTaskId(Long taskId);
+    List<TaskAssignmentRecord> findByStatus(AssignmentStatus status);
 }
