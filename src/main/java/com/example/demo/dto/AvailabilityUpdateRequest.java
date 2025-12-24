d0335ed405cac34034f97c9bd012e4b18fa2b18c
bala-amypo <bala@amypo.in>
2025-12-24 04:08:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AvailabilityUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+public class AvailabilityUpdateRequest {
+
+    private String availabilityStatus;
+
+    public AvailabilityUpdateRequest() {}
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setAvailabilityStatus(String availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+}
