fd1bac09a130910b381e799656243863011ce5c8
bala-amypo <bala@amypo.in>
2025-12-24 04:06:03 +0000

    User commit

--- src/main/java/com/example/demo/dto/TaskAssignmentRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dto;
-
-public class TaskAssignmentRequest {
-    private Long taskId;
-    
-    public Long getTaskId() { return taskId; }
-    public void setTaskId(Long taskId) { this.taskId = taskId; }
-}
