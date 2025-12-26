7ad5693134e1c24c96b263de8d4156dff4378968
bala-amypo <bala@amypo.in>
2025-12-26 17:25:13 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,17 +1,1 @@
-package com.example.demo.servlet;
-
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class HelloServlet extends HttpServlet {
-
-    @Override
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws IOException {
-
-        response.setContentType("text/plain");
-        response.getWriter().write("Hello from HelloServlet");
-    }
-}
+c
